d82dacbbb2f0ce686f1c5db779162b93026615f8
jhon <jhon.bocanegra26@gmail.com>
2024-08-28 17:21:43 -0500

    chore: Update project structure and add initial screens and components

---
+++ src/modules/core/screens/home.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen.tsx</Text>
+    </View>
+  )
+}
---
+++ src/modules/tabs/screens/tab.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export const TabScren = () => {
+  return (
+    <View>
+      <Text>TabScreen.tsx</Text>
+    </View>
+  );
+}
